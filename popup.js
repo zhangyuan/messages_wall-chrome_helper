fcb7ab026ef91d7cd09ecb9f6ad70b65c99fa669
Yuan Cheung <zhangyuan@users.noreply.github.com>
2014-09-14 12:15:30 +0800

    ui changes and version

--- popup.js
+++ popup.js
@@ -8,6 +8,13 @@ $(document).ready(function(){
   $("#sign_out").click(function(){
     chrome.storage.sync.set({token: null});
     chrome.storage.sync.set({host: null});
+    chrome.storage.sync.set({title: null});
+    localStorage["messages_wall:token"] = "";
+    localStorage["messages_wall:host"] = "";
+    localStorage["messages_wall:title"] = "";
+
+    $(".notice").text("已经退出");
+    console.log("sync: ", token, host, title);
   });
 
   $("#sign_in").submit(function(event){
@@ -30,6 +37,7 @@ $(document).ready(function(){
          localStorage["messages_wall:token"] = token;
          localStorage["messages_wall:host"] = host;
          localStorage["messages_wall:title"] = title;
+         $(".notice").text("登录成功");
          console.log("sync: ", token, host, title);
        });
      }
